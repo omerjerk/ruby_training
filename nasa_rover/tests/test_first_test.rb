ecff2d860304b2a3fd7792030a20beea34102c1f
Umair Khan <omerjerk@gmail.com>
2016-07-18 17:17:26 +0530

    add a new test case
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- nasa_rover/tests/test_first_test.rb
+++ nasa_rover/tests/test_first_test.rb
@@ -3,11 +3,19 @@ require "test/unit"
 
 class TestRover < Test::Unit::TestCase
 
-  def test_rover()
+  def test_rover_1()
     rover = Rover.new(1, 2, "N")
     navigate(rover, "LMLMLMLMM")
     assert_equal(1, rover.x)
     assert_equal(3, rover.y)
     assert_equal("N", rover.f)
+  end
+
+  def test_rover_2()
+  	rover = Rover.new(3, 3, "E")
+  	navigate(rover, "MMRMMRMRRM")
+  	assert_equal(5, rover.x)
+  	assert_equal(1, rover.y)
+  	assert_equal("E", rover.f)
   end
 end
