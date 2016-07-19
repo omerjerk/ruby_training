35b05f2cef05f0e48b9572fce5bcdc8b181a983a
Umair Khan <omerjerk@gmail.com>
2016-07-19 17:33:50 +0530

    Add unit tests
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

---
+++ api_server/tests/test_endpoints.rb
@@ -0,0 +1,31 @@
+ENV['RACK_ENV'] = 'test'
+
+require './api_server.rb'
+require 'test/unit'
+require 'rack/test'
+
+class APITest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_main
+    get '/generate'
+    assert last_response.ok?
+    assert_equal json_response("success"), last_response.body
+
+    5.times {
+    	get '/get'
+    	assert last_response.ok?
+    }
+
+    get '/get'
+    assert last_response.ok?
+    assert_equal json_response("key_not_available"), last_response.body
+
+  end
+
+end
+
